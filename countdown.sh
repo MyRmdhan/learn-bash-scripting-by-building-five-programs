4a62e214564f18f34109b63f4a6fd7e2aae5d339
MyRmdhan <maulanayazidr2@gmail.com>
2024-10-28 08:01:51 +0000

    MAULANA_YAZID_RAMADHAN--2404015

---
+++ countdown.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Program that counts down to zero from a given argument
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
+then
+while [[ $I -ge 0 ]]
+do
+  echo $I
+  (( I-- ))
+  sleep 1
+done
+else
+  echo Include a positive integer as the first argument.
+fi
